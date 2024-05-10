6214efa046834444238fb24c68bb4487fe0a3f41
Yy <akumakumu@gmail.com>
2024-05-10 15:26:59 +0700

    refactor navigation bar

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,5 +1,9 @@
 <script>
-    let menuHidden = true;
+    let isMenuOpen = false;
+
+    function toggleMenu() {
+      isMenuOpen = !isMenuOpen;
+    }
 </script>
 
 <nav class="
@@ -15,6 +19,7 @@
           text-gray-800
           font-kosugi
         ">
+
       <div>
         <a href="/">
             <img src="/saint-normal.png" alt="Saint" width="50" height="50" class="rounded-md">
@@ -23,19 +28,12 @@
 
       <!-- svelte-ignore a11y-click-events-have-key-events -->
       <!-- svelte-ignore a11y-no-static-element-interactions -->
-      <svg xmlns="http://www.w3.org/2000/svg" id="menu-button" class="h-6 w-6 cursor-pointer md:hidden block" fill="none" viewBox="0 0 24 24" stroke="currentColor" on:click={() => (menuHidden = !menuHidden)}>
+      <svg xmlns="http://www.w3.org/2000/svg" id="menu-button" class="h-6 w-6 cursor-pointer md:hidden block" fill="none" viewBox="0 0 24 24" stroke="currentColor" on:click={() => (isMenuOpen = !isMenuOpen)}>
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
       </svg>
 
-      {#if menuHidden}
-
-      <div class="hidden w-full md:flex md:items-center md:w-auto" id="menu">
-        <ul class="
-              pt-4
-              text-base text-gray-800
-              md:flex
-              md:justify-between 
-              md:pt-0">
+      <div class={`${isMenuOpen ? 'block' : 'hidden'} w-full md:flex md:items-center md:w-auto`} id="menu">
+        <ul class="pt-4 text-base text-gray-800 md:flex md:justify-between md:pt-0">
           <li>
             <a class="md:p-4 py-2 block hover:text-rose-400" href="/">Service</a>
           </li>
@@ -50,29 +48,6 @@
           </li>
         </ul>
       </div>
-      {:else}
-      <div class="w-full md:flex md:items-center md:w-auto" id="menu">
-        <ul class="
-              pt-4
-              text-base text-gray-800
-              md:flex
-              md:justify-between 
-              md:pt-0">
-          <li>
-            <a class="md:p-4 py-2 block hover:text-rose-400" href="/">Service</a>
-          </li>
-          <li>
-            <a class="md:p-4 py-2 block hover:text-rose-400" href="/">Project</a>
-          </li>
-          <li>
-            <a class="md:p-4 py-2 block hover:text-rose-400" href="/">About</a>
-          </li>
-          <li>
-            <a class="md:p-4 py-2 block hover:text-rose-400 text-rose-500" href="/">Contact</a>
-          </li>
-        </ul>
-      </div>
-      {/if}
     </nav>
 
 <style lang="postcss">
