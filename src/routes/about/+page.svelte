4159d4dc452a67766a67ef46a32bc64dd21b3e15
Yy <akumakumu@gmail.com>
2024-08-06 03:48:02 +0700

    add bio

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -8,19 +8,21 @@
     </header>
 
     <div class="w-full py-2">
-        <div class="flex flex-row items-start px-4 lg:px-32">
-            <div class="md:w-1/5 w-2/5">
-                <img src="/plainy34.jpg" alt="plainy" width="180" class="drop-shadow-xl">
+        <div class="flex flex-row md:items-start items-center    px-4 lg:px-32">
+            <div class="flex md:w-1/5 w-1/5">
+                <div>
+                    <img src="/plainy34.jpg" alt="plainy" width="180" class="drop-shadow-xl">
+                </div>
             </div>
 
-            <div class="md:w-4/5 w-3/5 md:p-4 px-4">
-                <p class="text-3xl font-kosugi">
+            <div class="md:w-4/5 w-4/5 md:p-4 px-4">
+                <p class="md:text-3xl text-2xl font-bold font-kosugi pb-2">
                     <span class="text-rose-600">./</span>
-                    <span class="text-gray-800">Nightingale</span>
+                    <span class="text-gray-800">Youne Baldea</span>
                 </p>
 
-                <p class="text-lg font-kosugi text-justify">
-                    <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus aperiam quisquam voluptas, porro impedit quidem magni dolor distinctio officiis non et vero similique harum. Cum similique possimus veritatis fugiat perspiciatis.</span>
+                <p class="md:text-lg text-sm font-kosugi text-justify">
+                    <span>Sorry if it little bit boring and bland, but i like this style. Anyway, I am usually known as kuu. I've been doing electronic repair full-time since 2016. In my free time, I like to tinker with Linux-related stuff, networking, or embedded systems in general. As for programming in my hobby projects, I mainly focus on writing firmware and services, given their close relation to electronics. Recently, I've been expanding my knowledge by delving into the field of artificial intelligence.</span>
                 </p>
             </div>
         </div>
