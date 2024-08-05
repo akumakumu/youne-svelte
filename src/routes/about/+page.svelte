3946cc72ac01888831f0b6ce023df60606303b8e
Yy <akumakumu@gmail.com>
2024-08-05 22:14:38 +0700

    init about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,8 +7,19 @@
         <NavBar />
     </header>
 
-    <div>
-        <p>Hello</p>
+    <div class="min-w-full py-2">
+        <div class="flex flex-row min-w-full px-32 p-2">
+            <div class="min-w-fit">
+                <img src="/plainy34.jpg" alt="plainy" width="180" class="drop-shadow-xl">
+            </div>
+
+            <div class="max-w-fit p-4">
+                <p class=" text-4xl font-kosugi">
+                    <span class="text-rose-600">./</span>
+                    <span class="text-gray-800">Nightingale</span>
+                </p>
+            </div>
+        </div>
     </div>
 </div>
 
