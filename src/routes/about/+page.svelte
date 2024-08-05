c597955a2edcfceadcf7b31871850b741e574497
Yy <akumakumu@gmail.com>
2024-08-05 22:25:47 +0700

    fix responsiveness

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,13 +7,13 @@
         <NavBar />
     </header>
 
-    <div class="min-w-full py-2">
-        <div class="flex flex-row min-w-full px-32 p-2">
-            <div class="min-w-fit">
+    <div class="w-full py-2">
+        <div class="flex flex-row items-start px-4 lg:px-32">
+            <div class="<w-></w->2/5">
                 <img src="/plainy34.jpg" alt="plainy" width="180" class="drop-shadow-xl">
             </div>
 
-            <div class="max-w-fit p-4">
+            <div class="w-3/5 p-4">
                 <p class=" text-4xl font-kosugi">
                     <span class="text-rose-600">./</span>
                     <span class="text-gray-800">Nightingale</span>
